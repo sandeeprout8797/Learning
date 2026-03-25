2255dc4e8e10dcad53b727bd8a450ca3e999db07
sandeeprout8797 <sandeeprout8797@gmail.com>
2026-03-25 13:08:05 +0530

    update

---
+++ project1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  let name = "Sandeep";
+
+  return (
+    <div className="Name">
+      <h1>H1: {name}</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ project1/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logosvg';
+import './App.css';
+
+function App() {
+    let name = "sandeep"
+
+    return {
+        <div class="App">
+        <h1> {name} </h1>
+        </div>
+    };
+}
